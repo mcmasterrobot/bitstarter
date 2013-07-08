4b2a44a608dee86fc3e1892f9188222c5beb757e
root <mail.SimpsonSciences.com>
2013-07-08 12:13:52 -0600

    updates

--- web.js
+++ web.js
@@ -1,15 +1,21 @@
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-    fs.readFile('index.html', function(err,data)){
-	if (err) throw err;
-	response.send(data);
+var buffer = new Buffer(64);
 
-    }
+app.get('/', function(request, response) {
+
+ 
+  fs.readFile('index.html', function(err,data){
+        if (err) throw err;
+        response.send(data);
+   });
 
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
