c60ebf2ed9d6fedc74c9861cb12e35b0a2baa39f
Andrew <mcmaster_robot@hotmail.com>
2013-07-20 16:49:09 -0600

    u

--- web.js
+++ web.js
@@ -4,12 +4,17 @@ var htmlfile="index.html"
 var fs = require('fs');
 
 
-var app = express.createServer(express.logger());
+//var app = express.createServer(express.logger());
+var app = connect.createServer().use(connect.static(__dirname + '/public'));
 app.get('/', function(request, response) {
 
 	var html = fs.readFileSync(htmlfile).toString();
 	response.send(html);
 });
+
+var connect = require("connect");
+
+
 
 
 
