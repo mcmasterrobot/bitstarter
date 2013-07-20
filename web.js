745bd7dff6d0009730aec1d70a2d68966786c5c4
Andrew <mcmaster_robot@hotmail.com>
2013-07-20 16:46:01 -0600

    web

--- web.js
+++ web.js
@@ -1,28 +1,19 @@
 
 var express = require('express');
+var htmlfile="index.html"
+var fs = require('fs');
+
 
 var app = express.createServer(express.logger());
-
-var fs = require('fs');
-var buffer = new Buffer(64);
-
-
 app.get('/', function(request, response) {
 
- 
-  fs.readFile('index.html', function(err,data){
-        if (err) throw err;
-     response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(data);
-      response.end();
-   });
-
-
+	var html = fs.readFileSync(htmlfile).toString();
+	response.send(html);
 });
 
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
